57e4b9b5e13b9df66c5a4993a306591a0f9da505
joaozin046 <joaoxpp2001@hotmail.com>
2023-04-09 19:09:57 -0300

    autenticação + intalação de bibliotecas
    Instalação do semantic ui react + css e adição do auth0 no projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton"; // Import the LoginButton component
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 
-function App() {
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoginButton/>
+      <LogoutButton/>
+      <Profile/>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import Auth0ProviderWithHistory from "./auth0ProviderWithHistory";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Auth0ProviderWithHistory>
-      <App />
-    </Auth0ProviderWithHistory>
-  </BrowserRouter>,
-  document.getElementById("root")
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}>
+    <App />
+  </Auth0Provider>,
 );
